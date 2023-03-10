67c15b60da5bdc95a813d06a274039c403c06977
JM <jmcafferata@gmail.com>
2023-03-09 22:11:43 -0300

    Actualicé el bot a la nueva versión

--- audiont.py
+++ audiont.py
@@ -5,8 +5,25 @@
 # import the necessary libraries // importar las librerías necesarias
 import os # operating system library that allows us to access the computer's file system // librería del sistema operativo que nos permite acceder al sistema de archivos del computador
 import openai # library used to access the OpenAI API // librería usada para acceder a la API de OpenAI
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters # library used to communicate with the Telegram bot // librería usada para comunicarse con el bot de Telegram
+from typing import DefaultDict, Optional, Set # library used to define the types of variables // librería usada para definir los tipos de variables
+from collections import defaultdict
+from pathlib import Path # library used to handle file paths // librería usada para manejar rutas de archivos
+from telegram.ext import (
+    Application,
+    CallbackContext,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    ExtBot,
+    TypeHandler,
+    InlineQueryHandler,
+    MessageHandler,
+    filters
+)
+from telegram.constants import ParseMode
 import config # import the config file // importar el archivo de configuración
+import asyncio # library used to handle asynchronous code // librería usada para manejar código asíncrono
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent # library used to communicate with the Telegram bot // librería usada para comunicarse con el bot de Telegram
 
 # set the OpenAI API key, so the code can access the API // establecer la clave de la API de OpenAI, para que el código pueda acceder a la API
 openai.api_key = config.openai_api_key
@@ -33,13 +50,14 @@ def convert_to_wav(audio_file):
     return new_file_name
 
 # main function that handles the audio files // función principal que maneja los archivos de audio
-def handle_audio(update, context):
+async def handle_audio(update, context):
     # Extract the audio file from the message // Extraer el archivo de audio del mensaje
-    audio_file = update.message.audio
+    new_file = await update.message.effective_attachment.get_file()
+    await new_file.download_to_drive()
+    file_path = Path(new_file.file_path).name
     # reply to the message, telling the user the audio file was received // responder al mensaje, diciendo al usuario que se recibió el archivo de audio
-    update.message.reply_text('Audio recibido. Esperá un toque que lo proceso. Te voy avisando.') 
-    # download the audio file // descargar el archivo de audio
-    file_path = audio_file.get_file().download()
+    await update.message.reply_text('Audio recibido. Esperá un toque que lo proceso. Te voy avisando.') 
+    
     # convert the audio file to a wav file // convertir el archivo de audio a un archivo wav
     wav_audio = open(convert_to_wav(file_path),"rb")
     # call the OpenAI API to get the text from the audio file // llamar a la API de OpenAI para obtener el texto del archivo de audio
@@ -48,13 +66,13 @@ def handle_audio(update, context):
     # print the text extracted from the audio file // imprimir el texto extraído del archivo de audio
     print("Transcription:\n"+transcription_object["text"])
     # reply with the text extracted from the audio file // responder con el texto extraído del archivo de audio
-    update.message.reply_text(transcription_object["text"]) 
+    await update.message.reply_text(transcription_object["text"]) 
     
     # call the OpenAI API to generate a summary of the voice note // llamar a la API de OpenAI para generar un resumen de la nota de voz
     summary_gpt_response = openai.Completion.create(
         model="text-davinci-003",
         # The prompt is the text that the model will use to generate a response. I add some things about me so that the model can generate a more personalized response // El prompt es el texto que el modelo usará para generar una respuesta. Agrego algunas cosas sobre mí para que el modelo pueda generar una respuesta más personalizada
-        prompt="Me acaban de enviar una nota de voz que dice lo siguiente: \n"+transcription_object["text"] + "\n --- FIN DE LA NOTA DE VOZ --- \n Crear, en español, un resumen breve de la nota de voz.",
+        prompt="Me acaban de enviar una nota de voz que dice lo siguiente: \n"+transcription_object["text"] + "\n --- FIN DE LA NOTA DE VOZ --- \n Crear, en español, un resumen breve pero específico de la nota de voz.",
         # The temperature is a number between 0 and 1 that determines how random the model's response will be // La temperatura es un número entre 0 y 1 que determina qué tan aleatoria será la respuesta del modelo
         temperature=0.7,
         # Tokens is kinda like the number of words the model will use to generate a response // Tokens es como el número de palabras que el modelo usará para generar una respuesta
@@ -70,11 +88,11 @@ def handle_audio(update, context):
     # print the decoded text // imprimir el texto decodificado
     print("Summary:\n"+decoded_summary_text)
     # Send the summary to the user // Enviar el resumen al usuario
-    update.message.reply_text(decoded_summary_text)
+    await update.message.reply_text(decoded_summary_text)
     # call the OpenAI API to generate a reply to the voice note // llamar a la API de OpenAI para generar una respuesta a la nota de voz
     reply_gpt_response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="Me acaban de enviar una nota de voz que dice lo siguiente: \n"+transcription_object["text"] + "\n --- FIN DE LA NOTA DE VOZ --- \n Crear una respuesta de mi parte, "+config.my_name + "\n\n Sobre mí: \n" + config.about_me_spanish,
+        prompt="Me acaban de enviar una nota de voz que dice lo siguiente: \n"+transcription_object["text"] + "\n --- FIN DE LA NOTA DE VOZ --- \n Crear una respuesta de mi parte, "+config.my_name + ", que primero repita los puntos principales de la nota para confirmar si se entendió.\n\n Sobre mí: \n" + config.about_me_spanish,
         temperature=0.7,
         max_tokens=2000,
         top_p=1,
@@ -88,21 +106,35 @@ def handle_audio(update, context):
     # print the decoded text // imprimir el texto decodificado
     print("Reply:\n"+decoded_reply_text)
     # Send the reply to the user // Enviar la respuesta al usuario
-    update.message.reply_text('Posible respuesta:')
-    update.message.reply_text(decoded_reply_text)
-    # delete the audio file // eliminar el archivo de audio
-    os.remove(file_path)
+    await update.message.reply_text('Posible respuesta:')
+    await update.message.reply_text(decoded_reply_text)
 
 
-# updater is used to communicate with the Telegram bot // updater se usa para comunicarse con el bot de Telegram
-updater = Updater(config.telegram_api_key) 
 
-# set the handler for audio files // establecer el manejador para archivos de audio
-updater.dispatcher.add_handler(MessageHandler(Filters.audio, handle_audio))
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Mandame audios.")
 
-# start the bot // iniciar el bot
-updater.start_polling()
+async def sendInfo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-# keep the code running // mantener el código en ejecución
-updater.idle()
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="No entiendo ese comando.")
 
+
+if __name__ == '__main__':
+    application = Application.builder().token(config.telegram_api_key).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    text_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), sendInfo)
+    application.add_handler(text_handler)
+
+    audio_handler = MessageHandler(filters.AUDIO, handle_audio)
+    application.add_handler(audio_handler)
+
+     # Other handlers
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    application.add_handler(unknown_handler)
+    
+    application.run_polling()

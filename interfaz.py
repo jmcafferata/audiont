3cc0faaeb12273a939a52f52ef9cb668fa834420
J. M. Cafferata <jmcafferata@gmail.com>
2023-08-13 17:09:17 -0300

    crazy

--- interfaz.py
+++ interfaz.py
@@ -3,13 +3,13 @@ from flask import Flask, flash, request, redirect, url_for, render_template, jso
 from werkzeug.utils import secure_filename
 import os
 import config
-import subprocess
 import random
-import openai
 import modules.transcription as transcription
+import logging
+
+logging.basicConfig(filename='app.log', level=logging.INFO)
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'users/'
 app.config['APPLICATION_ROOT'] = '/'+config.bot_code
 
 #try render index.html and debug
@@ -18,62 +18,14 @@ def index():
     app.logger.info("Index route accessed")
     return render_template('index.html')
 
-
-# Make sure to use a secret key for your application
-app.secret_key = "your-secret-key"
-
-
-# This code is used to upload a file to a user's folder. It is used in the vectorizar.html file to upload the file. It is also used in the app.py file to save the file in the user's folder.
-@app.route(app.config['APPLICATION_ROOT'] + '/vectorizar/<user_id>', methods=['GET', 'POST'])
-def upload_file(user_id):
-    if request.method == 'POST':
-        # Check if the file is present in the request
-        if 'file' not in request.files:
-            flash('No file found')
-            return redirect(request.url)
-        
-        file = request.files['file']
-
-        file_info = request.form.get('file_info')
-
-
-        # cehck if file is pdf or txt
-        if file.filename.split('.')[-1] not in ['pdf','txt']:
-            return render_template('error.html', user_id=user_id)
-        
-        # Check if the filename is empty
-        if file.filename == '':
-            flash('No file selected')
-            return redirect(request.url)
-
-        # Save the file to the user's folder
-        filename = secure_filename(file.filename)
-        user_folder = os.path.join(app.config['UPLOAD_FOLDER'], user_id,"to_vectorize")
-        # check if the user folder exists, if not create it
-        if not os.path.exists(user_folder):
-            os.makedirs(user_folder)       
-        
-        file.save(os.path.join(user_folder, filename))
-        # take the user to listo.html (in the templates folder)
-        
-
-        #store the file name and file info in a txt file in the to_vectorize folder
-        # the name of the txt file is the same as the name of the pdf file
-        with open(os.path.join(user_folder, 'metadata_'+filename.split('.')[0]+'.txt'), 'w') as f:
-            f.write(file_info)
-
-    
-
-        return render_template('listo.html', user_id=user_id)
-
-
-    return render_template('vectorizar.html', user_id=user_id)
-
-
-# Upload audio to server
-@app.route(app.config['APPLICATION_ROOT'] + '/upload_audio', methods=['POST'])
-def upload_audio():
+# Upload audio to server /audiont/upload_audio/<user_id>
+@app.route(app.config['APPLICATION_ROOT'] + '/upload_audio/<user_id>', methods=['POST'])
+def upload_audio(user_id):
     try:
+        # create the folder
+        folder_name = user_id
+        os.mkdir(os.path.join(app.config['UPLOAD_FOLDER'], folder_name))
+        
         # Check if the file is present in the request
         if 'file' not in request.files:
             return jsonify({'status': 'error', 'message': 'No file part in the request.'}), 400
@@ -89,24 +41,57 @@ def upload_audio():
         if not audio.filename.lower().endswith(('.mp3', '.wav', '.ogg')):
             return jsonify({'status': 'error', 'message': 'File type not allowed.'}), 400
         
-        # Save the audio file
-        audio.save(audio.filename)
-
-        # Split the audio in chunks of 30 seconds
-        audio_files = transcription.split_in_chunks(audio.filename)
-        transcription_text = transcription.transcribe_audios(audio_files, api_key=config.openai_api_key)
-        # Delete the audio files
-        for file in audio_files:
-            os.remove(file)
-
+        # Save the audio file to the user's folder
+        filename = secure_filename(audio.filename)
+        audio.save(os.path.join(app.config['UPLOAD_FOLDER'], folder_name, filename))
 
         
-        return jsonify({'status': 'success', 'message': transcription_text})
+        # message for the user to download the transcription when it is ready
+        message = 'La transcripción está lista. Puedes descargarla <a href="/'+config.bot_code+'/get_transcription/'+user_id+'">aquí</a>.'
+        
+        return jsonify({'status': 'success', 'message': message})
     except Exception as e:
         print(e)
         return jsonify({'status': 'error', 'error': str(e)})
 
+# /audiont/start_transcription/<user_id>
+@app.route(app.config['APPLICATION_ROOT'] + '/start_transcription/<user_id>', methods=['POST'])
+def start_transcription(user_id):
+    try:
+        #get folder
+        folder_name = user_id
 
+        #get audio file
+        audio_file = os.path.join(app.config['UPLOAD_FOLDER'], folder_name)
+        # Split the audio in chunks of 30 seconds
+        audio_files = transcription.split_in_chunks(audio_file)
+        transcription_text = transcription.transcribe_audios(audio_files, api_key=config.openai_api_key)
 
+        # Save the transcription to a txt file
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], folder_name, 'transcription.txt'), 'w') as f:
+            f.write(transcription_text)
+
+        # Delete the audio files
+        for file in audio_files:
+            os.remove(file)
+    except Exception as e:
+        print(e)
+
+# get transcription from server /audiont/get_transcription/<user_id>
+@app.route(app.config['APPLICATION_ROOT'] + '/get_transcription/<user_id>', methods=['GET'])
+def get_transcription(user_id):
+    try:
+        #get folder
+        folder_name = user_id
+
+        #get transcription
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], folder_name, 'transcription.txt'), 'r') as f:
+            transcription_text = f.read()
+        
+        #return transcription
+        return jsonify({'status': 'success', 'transcription': transcription_text})
+    except Exception as e:
+        print(e)
+        return jsonify({'status': 'error', 'error': str(e)})
 if __name__ == '__main__':
     app.run(debug=True)
